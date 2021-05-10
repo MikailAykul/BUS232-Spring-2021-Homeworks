cbfed9799ec6228fdcec531db9e0b86dcfcf4574
MikailAykul <81522751+MikailAykul@users.noreply.github.com>
2021-05-10 22:34:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 
-x = str(input("Enter a string plzz "))
-print (x[::-1].lower())
-
+x = int(input("What is your birth year: "))
+if x < 1900:
+    print("You are from Lost Generation.")
+elif x < 1927:
+    print("You are from Greatest Generation.")
+elif x < 1945:
+    print("You are from Silent Generation.")
+elif x < 1964:
+    print("You are from Baby boomers.")
+elif x < 1980:
+    print("You are from Generation X.")
+elif x < 1996:
+    print("You are from Millennials.")
+elif x < 2012:
+    print("You are from Generation Z.")
+else:
+    print("You are from Generation Alpha.")
 
     
